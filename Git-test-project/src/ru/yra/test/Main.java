b9fe8a68238d6f975decd32f66df358d856acc71
Yracuma yracuma@yandex.ru <Yracuma yracuma@yandex.ru>
2015-02-16 15:04:09 +0300

    new1

--- Git-test-project/src/ru/yra/test/Main.java
+++ Git-test-project/src/ru/yra/test/Main.java
@@ -2,7 +2,7 @@ package ru.yra.test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!!");
+		System.out.println("Hello from Git Hub!!!");
 		
 
 	}
